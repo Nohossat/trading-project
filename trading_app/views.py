f89e66129be04060c109419b2ad91ec5207f023b
Nohossat TRAORE <nohossat.traore@gmail.com>
2020-09-10 15:18:39 +0200

    finished flask app

--- trading_app/views.py
+++ trading_app/views.py
@@ -3,7 +3,10 @@ from flask import Flask, url_for, request, render_template
 from trading_app import app
 import data_wrangling.config as config
 from data_wrangling.db import Database
+import numpy as np
 import json
+from tensorflow import keras
+
 
 def get_time_series(symbol):
     db = Database()
@@ -33,12 +36,9 @@ def get_companies():
 @app.route('/')
 def index(title="Trading App"):
     companies = get_companies()
-    print(companies)
-
-    api = 12
     print(config.av_key)
 
-    symbol = "MSFT"
+    symbol = companies[0][0]
     dates, avg_prices = get_time_series(symbol)
     
     return render_template('home.html', title=title, 
@@ -50,5 +50,34 @@ def index(title="Trading App"):
 @app.route('/_reload_time_serie')
 def reload_time_serie():
     symbol = request.args.get('symbol', 0, type=str)
-    dates, avg_prices = get_time_series(symbol)
-    return json.dumps({"dates" : dates, "prices" : avg_prices})
+    dates, prices = get_time_series(symbol)
+    return json.dumps({"dates" : dates, "prices" : prices})
+
+@app.route('/predict')
+def predict():
+    symbol = request.args.get('symbol', 0, type=str)
+
+    # get dates, prices
+    dates, prices = get_time_series(symbol)
+    dates = json.loads(dates)
+    prices = np.array(json.loads(prices))
+
+    # get window_size and length of time series
+    prices_size = len(prices)
+    window = 3
+
+    # load model
+    model = keras.models.load_model(f'models/model_{symbol}.p')
+
+    # get last prices
+    last_prices = prices[prices_size - window:prices_size].reshape(1, window, 1)
+    prediction = model.predict(last_prices)
+    
+    if prediction > prices[prices_size - 1] + 0.05:
+        result = "You should buy some shares, it is going up tomorrow"
+    elif prediction < prices[prices_size - 1] + 0.05:
+        result = "You should sell, it is going down tomorrow"
+    else:
+        result = "You should do nothing, it is not moving much"
+
+    return json.dumps({"result" : result})
